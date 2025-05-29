4bbecf292fa356189714f37180a0afac632c2041
IbrahimAlakbarov06 <ibrahim.alakbarov2006@gmail.com>
2025-05-29 20:03:33 +0400

    cart dao updated

--- src/main/java/org/perfume/domain/repo/CartDao.java
+++ src/main/java/org/perfume/domain/repo/CartDao.java
@@ -1,4 +1,20 @@
 package org.perfume.domain.repo;
 
+import org.perfume.domain.entity.Cart;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface CartDao {
+
+    // İstifadəçinin səbətini tap (USER)
+    Optional<Cart> findUserId(Long id);
+
+    boolean existsByUserId(Long id);
+
+    // İstifadəçinin səbətini items ilə birlikdə gətir
+    @Query("select c from Cart c left join fetch c.items ci left join fetch ci.perfume where c.user.id = :userId")
+    List<Cart> findByUserIdWithItems(@Param("userId") Long userId);
 }
