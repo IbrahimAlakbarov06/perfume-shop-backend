129c17cee4893542fa95798ec72390767011f174
IbrahimAlakbarov06 <ibrahim.alakbarov2006@gmail.com>
2025-05-31 01:39:11 +0400

    Adding builder User entity

--- src/main/java/org/perfume/domain/entity/User.java
+++ src/main/java/org/perfume/domain/entity/User.java
@@ -2,6 +2,7 @@ package org.perfume.domain.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.perfume.model.enums.UserRole;
@@ -15,6 +16,7 @@ import java.util.Set;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class User {
 
     @Id
