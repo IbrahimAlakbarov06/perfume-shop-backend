27c038fbdad8488efaa4a82b953c9d92a5d589c9
IbrahimAlakbarov06 <ibrahim.alakbarov2006@gmail.com>
2025-06-21 18:56:20 +0400

    created security

---
+++ src/main/java/org/perfume/security/CustomUserDetailsService.java
@@ -0,0 +1,42 @@
+package org.perfume.security;
+
+import lombok.RequiredArgsConstructor;
+import org.perfume.domain.entity.User;
+import org.perfume.service.UserService;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.findUserByEmail(username);
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                user.isVerified(),
+                true,
+                true,
+                true,
+                getAuthorities(user)
+        );
+    }
+
+    private Collection<? extends GrantedAuthority> getAuthorities(User user) {
+        return Collections.singletonList(
+                new SimpleGrantedAuthority("ROLE_" + user.getRole().name())
+        );
+    }
+}
